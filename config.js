e891c798b8518333825fb1e16195365892464894
DominicDev <s0l0estudi4r@gmail.com>
2025-09-20 23:22:16 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'FIVE FINGER DEATH PUNCH ';
+export const nameSongs = [
+	"WASH IT ALL AWAY", /*1*/
+	"WRONG SIDE OF HEAVEN", /*2*/
+	"GONE AWAY", /*3*/
+	"BLUE ON BLACK", /*4*/
+	"COLD", /*5*/
+	"FAR FROM HOME", /*6*/
+	"A LITTLE BIT OFF", /*7*/
+	"THE PRIDE", /*8*/
+	"REMEMBER EVERYTHING", /*9*/
+	"M.I.N.E.", /*10*/
+	"INSIDE OUT", /*11*/
+	"WELCOME TO THE CIRCUS", /*12*/
+	"FAKE", /*13*/
+	"BAD COMPANY", /*14*/
+	"THE END ", /*15*/
+	"COMING DOWN", /*16*/
+	"BURN MF", /*17*/
+	"HELL TO PAY", /*18*/
+	"HOUSE OF THE RISING SUN", /*19*/
+	"LIFT ME UP", /*20*/
+]
